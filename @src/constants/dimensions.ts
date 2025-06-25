6f0071ddf53df6ecd78e5a6afe0a13a645f1531f
mindsgn <zookid@protonmail.com>
2025-06-26 00:03:38 +0200

    update

---
+++ @src/constants/dimensions.ts
@@ -0,0 +1,7 @@
+
+
+
+export {
+    width,
+    height
+}
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+      backgroundColor: "blue"
+    }
+});
