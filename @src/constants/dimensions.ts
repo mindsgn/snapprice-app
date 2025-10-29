f3822fdb0ff8a450741f0dc2d2df3b1e0166d0ef
mindsgn <zookid@protonmail.com>
2025-10-29 15:14:23 +0200

    update

--- @src/constants/dimensions.ts
+++
@@ -1,9 +0,0 @@
-import { Dimensions } from "react-native"
-
-const width = Dimensions.get("screen").width;
-const height = Dimensions.get("screen").height;
-
-export {
-    width,
-    height
-}
--- @src/hooks/replaceString.ts
+++
@@ -1,6 +0,0 @@
-const replaceString = (link: string) => {
-    const newLink = link.replace("{size}", "pdpxl");
-    return newLink
-}
-
-export { replaceString }
